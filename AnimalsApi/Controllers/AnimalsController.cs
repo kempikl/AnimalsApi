eadd7d6000849fda90856ff86957c9868ebb9c37
Łukasz Kempa <kempikl@gmail.com>
2024-04-26 23:33:06 +0200

    Initial commit

---
+++ AnimalsApi/Controllers/AnimalsController.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace AnimalsApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AnimalsController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public AnimalsController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetAnimals([FromQuery] string orderBy = "Name")
+        {
+            List<dynamic> animals = new List<dynamic>();
+            string query = $"SELECT * FROM Animal ORDER BY {orderBy}";
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+                connection.Open();
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    animals.Add(new
+                    {
+                        IdAnimal = reader["IdAnimal"],
+                        Name = reader["Name"],
+                        Description = reader["Description"],
+                        Category = reader["CATEGORY"],
+                        Area = reader["AREA"]
+                    });
+                }
+            }
+
+            return Ok(animals);
+        }
+
+        [HttpPost]
+        public IActionResult AddAnimal([FromBody] dynamic animal)
+        {
+            string query = "INSERT INTO Animal (Name, Description, CATEGORY, AREA) VALUES (@Name, @Description, @Category, @Area)";
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@Name", animal.Name);
+                command.Parameters.AddWithValue("@Description", animal.Description);
+                command.Parameters.AddWithValue("@Category", animal.Category);
+                command.Parameters.AddWithValue("@Area", animal.Area);
+
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                if (result < 0)
+                    return BadRequest();
+            }
+
+            return StatusCode(201);
+        }
+
+        [HttpPut("{idAnimal}")]
+        public IActionResult UpdateAnimal(int idAnimal, [FromBody] dynamic animal)
+        {
+            string query = "UPDATE Animal SET Name = @Name, Description = @Description, CATEGORY = @Category, AREA = @Area WHERE IdAnimal = @IdAnimal";
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@IdAnimal", idAnimal);
+                command.Parameters.AddWithValue("@Name", animal.Name);
+                command.Parameters.AddWithValue("@Description", animal.Description);
+                command.Parameters.AddWithValue("@Category", animal.Category);
+                command.Parameters.AddWithValue("@Area", animal.Area);
+
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                if (result < 0)
+                    return BadRequest();
+
+                return NoContent();
+            }
+        }
+
+        [HttpDelete("{idAnimal}")]
+        public IActionResult DeleteAnimal(int idAnimal)
+        {
+            string query = "DELETE FROM Animal WHERE IdAnimal = @IdAnimal";
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var command = new SqlCommand(query, connection);
+                command.Parameters.AddWithValue("@IdAnimal", idAnimal);
+                
+                connection.Open();
+                int result = command.ExecuteNonQuery();
+                if (result < 0)
+                    return BadRequest();
+
+                return NoContent();
+            }
+        }
+    }
+}
---
+++ AnimalsApi/Models/Animal.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AnimalsApi.Models
+{
+    public class Animal
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdAnimal { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Name { get; set; }
+
+        [MaxLength(200)]
+        public string Description { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Category { get; set; }
+
+        [Required]
+        [MaxLength(200)]
+        public string Area { get; set; }
+    }
+}
